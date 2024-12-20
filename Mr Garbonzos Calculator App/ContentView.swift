9e3de8d9c115213fd5c29815fe537a162d5deace
Dante Ausonio <ktefbre567@gmail.com>
2024-12-20 11:23:27 -0700

    Initial Commit

---
+++ Mr Garbonzos Calculator App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mr Garbonzos Calculator App
+//
+//  Created by Dante Ausonio on 12/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mr Garbonzos Calculator App/Mr_Garbonzos_Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mr_Garbonzos_Calculator_AppApp.swift
+//  Mr Garbonzos Calculator App
+//
+//  Created by Dante Ausonio on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Mr_Garbonzos_Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
